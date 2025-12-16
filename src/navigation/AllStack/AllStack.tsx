8497f7d198318f5f3a5c5de23ef1157e033defaa
Deependra-jhariya <deependrajhariya@gmail.com>
2025-12-16 10:40:52 +0530

    Resolve merge conflicts

--- src/navigation/AllStack/AllStack.tsx
+++ src/navigation/AllStack/AllStack.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CategoryScreen, DetailsScreen, SplashScreen } from '../../screens';
 
+
 const Stack = createNativeStackNavigator();
 const AllStack = () => {
   return (
